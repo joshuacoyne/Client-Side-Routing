d82a1bc25fcd039fd69a015332119fa292082a88
joshuacoyne <josh.j.coyne@gmail.com>
2018-03-12 19:56:17 -0400

    Client-Side-Routing -- COMPLETE

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,17 @@ import './index.css';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie'
 
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+
+
 ReactDOM.render(
-  <div>Application running, add your routing</div>,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route exact path='/' component={ MovieList } />
+      <Route path='/movies/:id' component={ Movie } />
+    </div>
+  </Router>
+  , document.getElementById('root')
 );
